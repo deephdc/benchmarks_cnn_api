cb1a1410a59761c492eec52671ba07c8842797fb
usmfz@student.kit.edu <usmfz@lsdf-28-131.scc.kit.edu>
2019-10-02 15:45:19 +0200

    standard options for training and evaluation

--- benchmarks_api/config.py
+++ benchmarks_api/config.py
@@ -7,37 +7,54 @@ from os import path
 
 # identify basedir for the package
 BASE_DIR = path.dirname(path.normpath(path.dirname(__file__)))
-DATA_DIR = /srv/benchmarks_api/data
-
+DATA_DIR = '/srv/benchmarks_api/data'
+MODEL_DIR = '/srv/benchmarks_api/models'
 
 # Training and predict(deepaas>=0.5.0) arguments as a dict of dicts 
-# with the following structure to feed the deepaas API parser:
-# (see also get_train_args() )
-# { 'arg1' : {'default': 1,       # default value
-#             'help': '',         # can be an empty string
-#             'required': False   # bool
-#             },
-#   'arg2' : {'default': 'value1',
-#             'choices': ['value1', 'value2', 'value3'],
-#             'help': 'multi-choice argument',
-#             'required': False
-#             }
-# }
-
-train_args = { 'model': {'default': 'resnet50',
-                        'choices': ['resnet50', 'alexnet'],
-                        'help': 'select CNN model for training',
+train_args = { 'model': {'default': 'alexnet',
+                        'choices': ['alexnet', 'resnet50'],
+                        'help': 'CNN model for training.',
                         'required': False
                         },
-               'n_gpus': {'default': 1,
-                          'help': 'Number of GPUs to train (one node only)',
+               'num_gpus': {'default': 1,
+                          'help': 'Number of GPUs to train (one node only).',
                           'required': False
-                          }
+                         },
+               'num_epochs': {'default': 5.0,
+                              'help': 'Number of epochs to train on.',
+                              'required': False
+                             },
+               'optimizer': {'default': 'sgd',
+                             'choices': ['sgd','momentum','rmsprop','adam'],
+                             'help': 'Optimizer to use.',
+                             'required': False
+                            },
+               'dataset': {'default': 'Synthetic data',
+                           'choices': ['Synthetic data', 'ImageNet', 'Cifar10'],
+                           'help': 'Dataset to perform training on. Synthetic \
+                            data are ramdomly generated ImageNet-like images.',
+                           'required': False
+                          },
+               'batch_size': {'default': 64,
+                              'help':'Batch size for each GPU.',
+                              'required': False
+                             }
 }
 
 # !!! deepaas>=0.5.0 calls get_test_args() to get args for 'predict'
-predict_args = { 'arg2': {'default': 1,
-                          'help': '',
+predict_args = {'model': {'default': 'alexnet',
+                          'choices': ['alexnet', 'resnet50'],
+                          'help': 'CNN model for training.',
                           'required': False
                          },
+               'num_gpus': {'default': 1,
+                            'help': 'Number of GPUs to train (one node only).',
+                            'required': False
+                           },
+               'dataset': {'default': 'Synthetic data',
+                           'choices': ['Synthetic data', 'ImageNet', 'Cifar10'],
+                           'help': 'Dataset to perform training on. Synthetic \
+                            data are ramdomly generated ImageNet-like images.',
+                           'required': False
+                          }
 }
