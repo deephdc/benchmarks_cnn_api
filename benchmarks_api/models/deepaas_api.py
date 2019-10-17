d23db131a48d5e5c9d0b0852ddfe84bec6817d2e
ace <usmfz@student.kit.edu>
2019-10-17 14:53:51 +0200

    preset amount for num_gpus enable training and evaluation on cpu (num_gpus = 0 option)

--- benchmarks_api/models/deepaas_api.py
+++ benchmarks_api/models/deepaas_api.py
@@ -8,7 +8,7 @@ import pkg_resources
 import yaml
 import json
 import os
-import tensorflow.test
+from tensorflow.python.client import device_lib
 
 from werkzeug.exceptions import BadRequest
 
@@ -17,6 +17,9 @@ import benchmarks_api.config as cfg
 import benchmark_cnn as benchmark
 import cnn_util
 
+
+local_gpus = []
+num_local_gpus = 0
 
 def get_metadata():
     """
@@ -87,7 +90,6 @@ def train(train_args):
                    "train_args": {},
                    "training": {}
                    }
-    run_results["train_args"] = train_args
 
     # Remove possible existing model files
     for f in os.listdir(cfg.MODEL_DIR):
@@ -105,8 +107,6 @@ def train(train_args):
               'optimizer': yaml.safe_load(train_args.optimizer),
               'local_parameter_device': 'cpu',
               'variable_update': 'parameter_server',
-              'train_dir': cfg.MODEL_DIR,
-              'benchmark_log_dir': cfg.DATA_DIR
               # 'benchmark_test_id': asdf,
               # 'log_dir': cfg.DATA_DIR,
               }
@@ -128,17 +128,22 @@ def train(train_args):
             kwargs['model'] = 'resnet50'
 
     # Check if a gpu is available, if not use cpu data format
-    gpu = tensorflow.test.gpu_device_name()
-    if not gpu:
-        kwargs['data_format'] = 'NHWC'
+    if num_local_gpus == 0 or kwargs['num_gpus'] == 0:
+        kwargs['device'] = 'cpu'
+        kwargs['data_format'] = 'NHWC'  # cpu data format
+        kwargs['num_gpus'] = 1  # Important: tensorflow uses this also to specify the number of CPUs
     else:
+        kwargs['device'] = 'gpu'
         kwargs['data_format'] = 'NCHW'
 
-    run_results["train_args"]["local_parameter_device"] = "cpu"
-    run_results["train_args"]["variable_update"] = "parameter_sever"
-    run_results["train_args"]["model"] = kwargs['model']
+
+    # Save training args but not directories
+    run_results["train_args"] = kwargs
+    kwargs['train_dir'] = cfg.MODEL_DIR
+    kwargs['benchmark_log_dir'] = cfg.DATA_DIR
 
     params = benchmark.make_params(**kwargs)
+
 
     # Setup and run the benchmark model
     try:
@@ -162,12 +167,15 @@ def train(train_args):
         avg_examples = result["value"]
         run_results["training"]["average_examples_per_sec"] = avg_examples
 
-    ### Perform evaluation if set true
+
+
+    ## Evaluation ##
     if yaml.safe_load(train_args.evaluation):
         run_results["evaluation"] = {}
 
         kwargs_eval = {'model': kwargs['model'],
                        'num_gpus': kwargs['num_gpus'],
+                       'device': kwargs['device'],
                        'data_format': kwargs['data_format'],
                        'benchmark_log_dir': kwargs['benchmark_log_dir'],
                        'train_dir': kwargs['train_dir'],
@@ -212,12 +220,27 @@ def get_train_args():
     Returns a dict of dicts to feed the deepaas API parser
     """
     train_args = cfg.train_args
+    global local_gpus, num_local_gpus
 
     # convert default values and possible 'choices' into strings
     for key, val in train_args.items():
         val['default'] = str(val['default'])  # yaml.safe_dump(val['default']) #json.dumps(val['default'])
         if 'choices' in val:
             val['choices'] = [str(item) for item in val['choices']]
+
+
+    # Adjust num_gpu option accordingly to available local devices
+    local_devices = device_lib.list_local_devices()
+    local_gpus = [x for x in local_devices if x.device_type == 'GPU']
+    num_local_gpus = len(local_gpus)
+
+    train_args['num_gpus']['choices'] = ['0']
+    if num_local_gpus == 0:
+        train_args['num_gpus']['default'] = '0'
+    else:
+        train_args['num_gpus']['default'] = '1'
+        for i in range(num_local_gpus): train_args['num_gpus']['choices'].append(str(i+1))
+
 
     return train_args
 
