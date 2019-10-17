3e68f1d035970918934295e84b4a456ed4e417ce
ace <usmfz@student.kit.edu>
2019-10-17 11:14:23 +0200

    make some arguments mandatory, change some information, set evaluation default to true

--- benchmarks_api/config.py
+++ benchmarks_api/config.py
@@ -9,6 +9,7 @@ from os import path
 BASE_DIR = path.dirname(path.normpath(path.dirname(__file__)))
 DATA_DIR = '/srv/benchmarks_api/data'
 MODEL_DIR = '/srv/benchmarks_api/models'
+BENCHMARK_DIR = '/srv/tf_cnn_benchmarks'
 
 # Training and predict(deepaas>=0.5.0) arguments as a dict of dicts 
 train_args = { 'model': {'default': 'alexnet',
@@ -16,14 +17,14 @@ train_args = { 'model': {'default': 'alexnet',
                         'help': 'CNN model for training. ResNet model is chosen \
                         according to dataset (ImageNet - ResNet50, Cifar10 - \
                         ResNet56).',
-                        'required': False
+                        'required': True
                         },
                'num_gpus': {'default': 1,
-                          'help': 'Number of GPUs to train (one node only).',
-                          'required': False
-                         },
+                          'help': 'Number of GPUs to train (one node only). If zero, cpu is used',
+                          'required': True
+                           },
                'num_epochs': {'default': 5.0,
-                              'help': 'Number of epochs to train on.',
+                              'help': 'Number of epochs to train on (float value. < 1.0 allowed).',
                               'required': False
                              },
                'optimizer': {'default': 'sgd',
@@ -35,18 +36,18 @@ train_args = { 'model': {'default': 'alexnet',
                            'choices': ['Synthetic data', 'imagenet', 'cifar10'],
                            'help': 'Dataset to perform training on. Synthetic \
                             data are ramdomly generated ImageNet-like images.',
-                           'required': False
+                           'required': True
                           },
                'batch_size': {'default': 64,
                               'help':'Batch size for each GPU.',
                               'required': False
                              },
-               'evaluation': {'default': False,
+               'evaluation': {'default': True,
                               'choices': [False, True],
-                              'help': 'Perform automated prediction after the \
+                              'help': 'Perform evaluation after the \
                               benchmark in order to get accuracy results (only \
-                              meaningful on real data sets).',
-                              'required': False
+                              meaningful on real data sets!).',
+                              'required': True
                              }
              }
 
